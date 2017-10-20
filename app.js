af9419eef85aa5cdd459d5347f6b7be5313c8984
Ajay Kotian <ajay.kotian@csusb.edu>
2017-10-20 09:06:17 -0700

    Added more tests.

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+// Set the environment variable NODE_ENV to production, to run the app in production mode.
+
 
 // Routes
 app.use('/', routes);
