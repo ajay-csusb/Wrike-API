0a0f7ca634dc0947ebfe62038cfa54e85e47ad9f
Ajay Kotian <ajay.kotian@csusb.edu>
2018-03-12 14:11:12 -0700

    Broadcast events to Drupal sites.

---
+++ controllers/eventsController.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const async = require('async');
+
+let query = {};
+
+// Send event updates to Drupal installations.
+function broadcastEventUpdates(domains, callback) {
+  request.get(`https://${domains}/update-events`, { qs: query }, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+    callback(null, body);
+  });
+}
+
+// Broadcast event to all installation on Acquia.
+function broadcastEvents(req, res) {
+  query = req.query;
+  async.map(req.body, broadcastEventUpdates, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json({ message: 'Event update request sent to all sites.' });
+}
+
+module.exports = {
+  broadcastEvents,
+};
--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,10 @@ const router = express.Router();
 
 // Define controllers
 const indexController = require('../controllers/indexController');
+const eventsController = require('../controllers/eventsController');
 
 router.get('/', indexController.home);
 router.get('/get/wrike/projects', indexController.getProjects);
+router.post('/api/update-events', eventsController.broadcastEvents);
 
 module.exports = router;
